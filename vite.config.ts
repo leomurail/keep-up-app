bfec4b05a338495c5232b30c3b6a8e27e6be67aa
Léo Murail <leo.murai85@gmail.com>
2026-01-26 12:09:42 +0100

    release-next:  Introduce `getConfig` utility for centralized environment variable access, updating Vite configuration and API client to use dynamic environment values.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    envPrefix: ['VITE_', 'APP_'],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    host: true,
-    port: Number(process.env.APP_PORT) || 3000,
-    allowedHosts: ["app.keep-up.traefik.me"],
-},
+    server: {
+      host: true,
+      port: Number(env.APP_PORT) || 3000,
+      allowedHosts: [env.APP_DOMAIN || ""],
+    },
+  }
 })
